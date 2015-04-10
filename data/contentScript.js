3b3bb9f08b884570a1c7b90cd78b6d414d049d73
Przemysław Wojnowski <esperanto@cumego.com>
2015-04-10 16:56:39 +0200

    Refactor to Composed Method.

--- data/contentScript.js
+++ data/contentScript.js
@@ -1,9 +1,12 @@
 // Get selection and limit to only the first word, because vortaro.net
-// doesn't support sentences. Also, remove -n and -j and present verbs
-// as infinitives (replacing -as/is/os with -i).
-function getSelectionText() {
-    var text = window.getSelection().toString();
-    text = text.split(/\s+/, 1)[0].toLowerCase();
+// doesn't support sentences.
+function firstWord(text) {
+    return text.split(/\s+/, 1)[0].toLowerCase();
+}
+
+// Remove -n and -j and present verbs as infinitives
+// (replacing -as/is/os with -i).
+function normalize(text) {
     if (text.endsWith("n") && text !== "kun") {
         text = text.substring(0, text.length-1);
     }
@@ -14,6 +17,12 @@ function getSelectionText() {
         text = text.substring(0, text.length-2) + "i";
     };
     return text;
+}
+
+function getSelectionText() {
+    var text = window.getSelection().toString();
+    text = firstWord(text);
+    return normalize(text);
 }
 
 self.on("context", function () {
